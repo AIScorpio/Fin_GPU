6c84ae9a98c50ec1c2080b14a7fe4d7e1bf0fd2b
Leon Xing Li <109344283+AIScorpio@users.noreply.github.com>
2025-04-21 03:28:12 +0800

    Add files via upload

--- src/American_option.py
+++ src/American_option.py
@@ -1,15 +1,16 @@
 from models.mc import MonteCarloBase, hybridMonteCarlo
 from models.longstaff import LSMC_Numpy, LSMC_OpenCL
 import models.benchmarks as bm
-from models.pso import PSO_Numpy, PSO_OpenCL
+from models.pso import PSO_Numpy, PSO_OpenCL_hybrid, PSO_OpenCL_scalar, PSO_OpenCL_scalar_oneKnl, PSO_OpenCL_vec
 from models.utils import checkOpenCL
     
 
 if __name__ == "__main__":
     checkOpenCL()
 
-    # S0, r, sigma, T, nPath, nPeriod, K, opttype, nFish = 100.0, 0.03, 0.3, 1.0, 20000, 200, 110.0, 'P', 1000
+    # S0, r, sigma, T, nPath, nPeriod, K, opttype, nFish = 100.0, 0.03, 0.3, 1.0, 20000, 200, 100.0, 'P', 1000
     S0, r, sigma, T, nPath, nPeriod, K, opttype, nFish = 100.0, 0.03, 0.3, 1.0, 2**14, 2**8, 110.0, 'P', 2**10
+    # S0, r, sigma, T, nPath, nPeriod, K, opttype, nFish = 100.0, 0.03, 0.3, 1.0, 400, 200, 122.0, 'P', 100
     mc = hybridMonteCarlo(S0, r, sigma, T, nPath, nPeriod, K, opttype, nFish)
     print(f'{nPath} paths, {nPeriod} periods, {nFish} particles.\n')
 
@@ -24,18 +25,27 @@ if __name__ == "__main__":
     lsmc_np = LSMC_Numpy(mc)
     lsmc_np.longstaff_schwartz_itm_path_fast()
 
-    lsmc_cl = LSMC_OpenCL(mc, preCalc="optimized", inverseType='CA')
+    # lsmc_cl = LSMC_OpenCL(mc, preCalc="optimized", inverseType='CA')
+    lsmc_cl = LSMC_OpenCL(mc, preCalc=None, inverseType='GJ')
     lsmc_cl.longstaff_schwartz_itm_path_fast_hybrid()
 
     # pso
-    pso_np = PSO_Numpy(mc, nFish, mc.costPsoAmerOption_np)
+    pso_np = PSO_Numpy(mc, nFish)
     pso_np.solvePsoAmerOption_np()
 
-    pso_cl = PSO_OpenCL(mc, nFish, mc.costPsoAmerOption_cl)
+    pso_cl_hybrid = PSO_OpenCL_hybrid(mc, nFish)
+    pso_cl_hybrid.solvePsoAmerOption_cl()
+
+    pso_cl = PSO_OpenCL_scalar(mc, nFish)
     pso_cl.solvePsoAmerOption_cl()
 
+    # pso_cl2 = PSO_OpenCL_scalar_oneKnl(mc, nFish)
+    # pso_cl2.solvePsoAmerOption_cl()
+
+    pso_cl_vec = PSO_OpenCL_vec(mc, nFish)
+    pso_cl_vec.solvePsoAmerOption_cl()
+
     # clear up memory
-    pso_cl.cleanUp()
     mc.cleanUp()
     
     
